33c30a849bc1cbec645b15a66bc58f0bde825ae7
Loki99911 <nikita_kresik@i.ua>
2023-12-02 12:53:37 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/ValueSwap/",
   // server: {
   //   proxy: {
   //     "/api": {
-  //       target: "https://api.coingecko.com", 
+  //       target: "https://api.coingecko.com",
   //       changeOrigin: true,
   //       rewrite: (path) => path.replace(/^\/api/, ""),
   //     },
